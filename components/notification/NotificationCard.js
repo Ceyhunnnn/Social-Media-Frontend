3b61af212eb68a2bd98b82eef8113ac00fdc147c
Ceyhun-gl <ceyhungul148@gmail.com>
2024-05-29 21:33:17 +0300

    bg color removed

--- components/notification/NotificationCard.js
+++ components/notification/NotificationCard.js
@@ -3,7 +3,7 @@ import Profile from "../icons/Profile";
 const NotificationCard = ({ title, createdAt, content }) => {
   return (
     <div className="flex justify-between items-center border-b border-gray-30 pb-3">
-      <div className="flex gap-x-3 items-center bg-red-500 ">
+      <div className="flex gap-x-3 items-center">
         <Profile width={40} height={40} />
         <h1 className="text-sm text-black-90">{title}</h1>
         <h2 className="text-xs text-black-60">{content}</h2>
