1ef9854f894337bdfe387afed5efb953cb3907e3
Ceyhun-gl <ceyhungul148@gmail.com>
2024-06-13 18:18:33 +0300

    api structure changed

--- service/api.js
+++ service/api.js
@@ -1,11 +1,22 @@
 import axiosClient from "./axios";
+import { getSession } from "next-auth/react";
 
+export const getHeaders = async () => {
+  const session = await getSession();
+  let headers = {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    Authorization: `Bearer ${session?.user?.token}`,
+  };
+  return headers;
+};
 export const api = async ({ type = "get", body, url }) => {
   try {
     const response = await axiosClient.request({
       url,
       method: type,
       data: body,
+      headers: await getHeaders(),
     });
     return { response: response?.data, responseHeaders: response?.headers };
   } catch (err) {
--- service/axios.js
+++ service/axios.js
@@ -1,19 +1,7 @@
 import axios from "axios";
-import { getSession } from "next-auth/react";
 
-export const getHeaders = async () => {
-  const session = await getSession();
-  let headers = {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": "*",
-    Authorization: `Bearer ${session?.user?.token}`,
-  };
-  return headers;
-};
-
-const axiosClient = await axios.create({
+const axiosClient = axios.create({
   baseURL: process.env.API_URL,
-  headers: await getHeaders(),
   withCredentials: false,
 });
 
