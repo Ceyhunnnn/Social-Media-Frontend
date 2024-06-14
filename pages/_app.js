c8f712b1a5af56c6a5fe403c0625af5b32c5e5e1
Ceyhun-gl <ceyhungul148@gmail.com>
2024-06-14 09:36:56 +0300

    materil tailwind installed

--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,18 @@ import { Provider } from "react-redux";
 import { SessionProvider } from "next-auth/react";
 
 import "@/styles/globals.css";
-
+import { ThemeProvider } from "@material-tailwind/react";
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
   return (
-    <SessionProvider session={session}>
-      <Provider store={store}>
-        <Component {...pageProps} />
-      </Provider>
-    </SessionProvider>
+    <ThemeProvider>
+      <SessionProvider session={session}>
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      </SessionProvider>
+    </ThemeProvider>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+const withMT = require("@material-tailwind/react/utils/withMT");
+module.exports = withMT({
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./layouts/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -28,4 +30,4 @@ module.exports = {
     },
   },
   plugins: [],
-};
+});
