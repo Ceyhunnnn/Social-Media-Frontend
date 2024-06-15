70877394226cb7583cac685be97ec7223c6b2445
Ceyhun-gl <ceyhungul148@gmail.com>
2024-06-15 13:27:32 +0300

    confirm dialog added

---
+++ components/confirmDialog/ConfirmDialog.js
@@ -0,0 +1,46 @@
+import {
+  Button,
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
+} from "@material-tailwind/react";
+import { useSelector } from "react-redux";
+import { setDialogValue } from "@/store/globalActions";
+
+const ConfirmDialog = ({ openKey, title, desc, confirm }) => {
+  const dialog = useSelector((state) => state.dialog.value);
+  const handleOpen = async () => {
+    setDialogValue(null);
+  };
+
+  return (
+    <Dialog open={dialog === openKey} handler={handleOpen}>
+      <DialogHeader>{title}</DialogHeader>
+      <DialogBody>{desc}</DialogBody>
+      <DialogFooter>
+        <Button
+          variant="text"
+          color="red"
+          onClick={handleOpen}
+          className="mr-1"
+        >
+          <span>Cancel</span>
+        </Button>
+        <Button
+          variant="text"
+          color="green"
+          onClick={() => {
+            if (confirm) {
+              confirm();
+            }
+            handleOpen();
+          }}
+        >
+          <span>Confirm</span>
+        </Button>
+      </DialogFooter>
+    </Dialog>
+  );
+};
+export default ConfirmDialog;
--- components/header/Header.js
+++ components/header/Header.js
@@ -6,32 +6,46 @@ import Input from "../input/Input";
 import routes from "@/routes/routes";
 import { signOut } from "next-auth/react";
 import { useRouter } from "next/router";
+import { setDialogValue } from "@/store/globalActions";
+import ConfirmDialog from "../confirmDialog/ConfirmDialog";
 
 const Header = () => {
   const router = useRouter();
+  const logout = () => {
+    signOut({ redirect: false }).then(() => {
+      router.push(routes.auth);
+    });
+  };
   return (
-    <header className="bg-white h-20 w-full flex justify-center items-center px-2 sticky top-0 shadow">
-      <div className="max-w-7xl flex justify-between w-full py-5">
-        <Link className="flex gap-x-4 items-center" href={routes.feed}>
-          <Logo />
-          <p className="text-lg font-semibold">Social</p>
-        </Link>
-        <div className="max-w-[582px] w-full mr-16">
-          <Input type="text" icon={<Search />} placeholder="Search" />
+    <>
+      <header className="bg-white h-20 w-full flex justify-center items-center px-2 sticky top-0 shadow">
+        <div className="max-w-7xl flex justify-between w-full py-5">
+          <Link className="flex gap-x-4 items-center" href={routes.feed}>
+            <Logo />
+            <p className="text-lg font-semibold">Social</p>
+          </Link>
+          <div className="max-w-[582px] w-full mr-16">
+            <Input type="text" icon={<Search />} placeholder="Search" />
+          </div>
+          <div
+            className="flex gap-x-4 cursor-pointer text-sm text-black-80 items-center"
+            onClick={() => {
+              // setDialogValue("exitConfirm");
+              logout();
+            }}
+          >
+            <p>Logout</p>
+            <Person />
+          </div>
         </div>
-        <div
-          className="flex gap-x-4 cursor-pointer text-sm text-black-80 items-center"
-          onClick={() => {
-            signOut({ redirect: false }).then(() => {
-              router.push(routes.auth); // Redirect to the dashboard page after signing out
-            });
-          }}
-        >
-          <p>Logout</p>
-          <Person />
-        </div>
-      </div>
-    </header>
+      </header>
+      {/* <ConfirmDialog
+        openKey="exitConfirm"
+        title="Exit from app"
+        desc="Are you sure exit from application?"
+        confirm={logout}
+      /> */}
+    </>
   );
 };
 
