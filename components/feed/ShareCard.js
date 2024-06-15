f4f4cb1b0bfd3922060db6fc5caa2bc3371eea85
Ceyhun-gl <ceyhungul148@gmail.com>
2024-06-15 15:02:47 +0300

    share button condition edited

--- components/feed/ShareCard.js
+++ components/feed/ShareCard.js
@@ -10,25 +10,27 @@ const ShareCard = () => {
   const { user } = useSelector((state) => state.globalState);
   const [text, setText] = useState(null);
   const createPost = async () => {
-    const body = {
-      userId: user?._id,
-      firstName: user?.firstName,
-      lastName: user?.lastName,
-      title: user?.title,
-      content: text,
-    };
-    const { response, error } = await api({
-      url: "createUserPost",
-      body,
-      type: "post",
-    });
+    if (text) {
+      const body = {
+        userId: user?._id,
+        firstName: user?.firstName,
+        lastName: user?.lastName,
+        title: user?.title,
+        content: text,
+      };
+      const { response, error } = await api({
+        url: "createUserPost",
+        body,
+        type: "post",
+      });
 
-    if (!error) {
-      toast.success(response?.message);
-    } else {
-      toast.error(err);
+      if (!error) {
+        toast.success(response?.message);
+      } else {
+        toast.error(err);
+      }
+      setText("");
     }
-    setText(null);
   };
   return (
     <div className="w-full flex flex-col gap-y-4 bg-white rounded-normal h-[154px] border border-gray-30 px-7 py-5">
@@ -39,7 +41,7 @@ const ShareCard = () => {
           onChange={(e) => setText(e.target.value)}
           value={text}
           placeholder="What's on your mind?"
-          className="border-b border-gray-40 placeholder:text-black-30 text-sm text-black-30 w-full outline-none pb-5 mt-4"
+          className="border-b border-gray-40 placeholder:text-black-30 text-sm text-black-30 w-full outline-none pb-5 mt-4 resize-none"
         />
       </div>
       <div className="flex items-center justify-between pl-14 mt-3 ">
@@ -51,7 +53,7 @@ const ShareCard = () => {
           disabled={!text}
           type="button"
           onClick={createPost}
-          className="bg-blue-90  lowercase  text-white text-sm rounded-full flex justify-center items-center w-12 h-8"
+          className="bg-blue-90    text-white text-sm rounded-full flex justify-center items-center w-12 h-8"
         >
           Post
         </button>
